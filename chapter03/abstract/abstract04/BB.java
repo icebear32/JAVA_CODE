4817659978d71c89f492b2b3cfb031f39c8cea38
icebear32 <1727693137@qq.com>
2022-11-13 23:04:12 +0800

    第十九次上传

---
+++ chapter03/abstract/abstract04/BB.java
@@ -0,0 +1,10 @@
+public class BB extends Template {
+	//计算任务
+	@Override
+	public void job() { 	//重写 job 方法
+		long num = 0;
+		for (long i = 0; i <= 80000; i++) {
+			num *= i;
+		}
+	}
+}
---
+++ chapter03/abstract/abstract04/TestTemplate.java
@@ -0,0 +1,11 @@
+public class TestTemplate {
+	public static void main(String[] args) {
+		AA a = new AA();
+		//a.job();
+		a.calculateTime();
+
+		BB b = new BB();
+		//b.job();
+		b.calculateTime();
+	}
+}
